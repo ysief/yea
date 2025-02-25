0ce77d467384468da9487ed59a1995ca415bf911
ysief <fzzure160@gmail.com>
2025-02-25 20:13:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -122,7 +122,7 @@ function getFarPosition(containerRect, windowWidth, windowHeight, buttonWidth, b
 
 yesBtn.addEventListener('click', () => {
     title.innerHTML = "YIIPPPIIEEE!!!";
-    document.querySelector('img').src = "https://media.tenor.com/gUiu1zyxfzYAAAAi/good-night-kiss-kiss.gif";
+    document.querySelector('img').src = "https://images.app.goo.gl/aFoaFEG1AqwPLzG46";
     noBtn.style.display = 'none';
     yesBtn.style.display = 'none';
     bgMusic.play();
@@ -134,7 +134,7 @@ noBtn.addEventListener('click', () => {
         title.innerHTML = messages[noCount - 1].text;
         document.querySelector('img').src = messages[noCount - 1].image;
     } else {
-        title.innerHTML = "TAPI BOONG HEHEHE";
+        title.innerHTML = "TAPI BOONG WKAKWKAKWKA";
         if (!noBtn.classList.contains('running')) {
             noBtn.classList.add('running');
         }
